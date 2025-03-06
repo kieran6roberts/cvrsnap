1d02a358729ce46acbd39fd4e5ef6c34ff0d48c1
kieran <kieran6roberts@gmail.com>
2025-03-06 15:20:02 +0000

    refactor: background image placeholder UI

--- packages/frontend/app/features/editor/utils/index.ts
+++ packages/frontend/app/features/editor/utils/index.ts
@@ -1,1 +1,8 @@
 export const decimalToPercentage = (decimal: number) => decimal * 100;
+
+export function splitAndCapitalizeCamelCase(str: string) {
+  return str
+    .split(/(?=[A-Z])/)
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
