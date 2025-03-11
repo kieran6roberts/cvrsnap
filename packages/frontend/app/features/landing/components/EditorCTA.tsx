bb5da7c345f2f2cfd8b5e114950a5a2ec51b5bf6
Kieran Roberts <kieran6roberts@gmail.com>
2025-03-11 17:00:31 +0000

    Add new landing cta (#76)
    * Update README.md
    
    * feat: add new landing cta + misc color contrast improvements
    
    * chore: remove unused style

---
+++ packages/frontend/app/features/landing/components/EditorCTA.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@mantine/core';
+import { motion } from 'motion/react';
+import { Link } from 'react-router';
+import classes from '~/features/landing/styles/editorcta.module.css';
+
+export function EditorCTA() {
+  return (
+    <div className={classes.container}>
+      <motion.h2
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        className={classes.title}
+      >
+        Ready to build?
+      </motion.h2>
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+        className={classes.copy}
+      >
+        Don't put off that blog post that you've been meaning to finish. Create a cover image in minutes with CvrSnap so
+        you can publish faster.
+      </motion.p>
+      <Button component={Link} to="/create" size="sm" hiddenFrom="md" variant="white" viewTransition>
+        Start for free
+      </Button>
+      <Button component={Link} to="/create" size="md" visibleFrom="md" variant="white" viewTransition>
+        Start for free
+      </Button>
+    </div>
+  );
+}
