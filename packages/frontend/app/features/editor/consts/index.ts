c81f92f9535b7e15dc2eaf4a87630817dc6168d6
kieran <kieran6roberts@gmail.com>
2025-03-20 09:57:24 +0000

    refactor: add more gradient options

--- packages/frontend/app/features/editor/consts/index.ts
+++ packages/frontend/app/features/editor/consts/index.ts
@@ -33,6 +33,14 @@ export const GRADIENT_PRESETS: {
     }
   },
   {
+    id: 'green-blue',
+    name: 'Green blue',
+    value: {
+      colors: ['#78FFD6', '#3b3e39'],
+      type: 'linear-gradient'
+    }
+  },
+  {
     id: 'bunch-of-grapes',
     name: 'Bunch of grapes',
     value: {
@@ -57,34 +65,10 @@ export const GRADIENT_PRESETS: {
     }
   },
   {
-    id: 'buzz',
-    name: 'Buzz',
-    value: {
-      colors: ['#051937', '#004d7a', '#008793', '#00bf72', '#a8eb12'],
-      type: 'linear-gradient'
-    }
-  },
-  {
-    id: 'blossom',
-    name: 'Blossom',
-    value: {
-      colors: ['#C5C4B3', '#CE3468'],
-      type: 'linear-gradient'
-    }
-  },
-  {
     id: 'midnight',
     name: 'Midnight',
     value: {
       colors: ['#6B11CD', '#1B1C23'],
-      type: 'linear-gradient'
-    }
-  },
-  {
-    id: 'land-and-sea',
-    name: 'Land and sea',
-    value: {
-      colors: ['#7E8AF3', '#49A24D'],
       type: 'linear-gradient'
     }
   },
@@ -109,6 +93,54 @@ export const GRADIENT_PRESETS: {
     name: 'Purple haze',
     value: {
       colors: ['#E2D8DF', '#DBC1DE'],
+      type: 'linear-gradient'
+    }
+  },
+  {
+    id: 'beachfront',
+    name: 'Beachfront',
+    value: {
+      colors: ['#E1F5C4', '#EDE574'],
+      type: 'linear-gradient'
+    }
+  },
+  {
+    id: 'just-cool',
+    name: 'Just cool',
+    value: {
+      colors: ['#A5FECB', '#20BDFF', '#5433FF'],
+      type: 'linear-gradient'
+    }
+  },
+  {
+    id: 'blue-lake',
+    name: 'Blue lake',
+    value: {
+      colors: ['#6DD5ED', '#2193B0'],
+      type: 'linear-gradient'
+    }
+  },
+  {
+    id: 'cotton-candy',
+    name: 'Cotton candy',
+    value: {
+      colors: ['#FBC7D4', '#9796F0'],
+      type: 'linear-gradient'
+    }
+  },
+  {
+    id: 'buzz',
+    name: 'Buzz',
+    value: {
+      colors: ['#051937', '#004d7a', '#008793', '#00bf72', '#a8eb12'],
+      type: 'linear-gradient'
+    }
+  },
+  {
+    id: 'blossom',
+    name: 'Blossom',
+    value: {
+      colors: ['#C5C4B3', '#CE3468'],
       type: 'linear-gradient'
     }
   }
